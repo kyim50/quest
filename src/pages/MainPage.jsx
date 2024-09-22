c7b7e1f254e2af580addf7524352233e067111e8
sahdeek <sahdeek.thompson@gmail.com>
2024-09-22 11:21:42 -0500

    add side button icons

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './MainPage.css';
+
 import NavigationBar from '../components/navigation-bar/NavigationBar';
 import Feed from '../components/feed/Feed';
-import Map from "../components/map/Map"
+import Map from '../components/map/Map';
+
+import SearchIcon from '../assets/icons/search_24dp_E8EAED_FILL1_wght400_GRAD0_opsz24.svg?react';
+import DropDownIcon from '../assets/icons/arrow_drop_down_24dp_E8EAED_FILL1_wght400_GRAD0_opsz24.svg?react';
+import CreateQuestIcon from '../assets/icons/new_window_24dp_E8EAED_FILL1_wght400_GRAD0_opsz24.svg?react';
 
 function MainPage() {
   return (
@@ -15,14 +20,42 @@ function MainPage() {
       </div>
       <div className="right-panel">
         <Map />
-        <div></div>
+        <div className="actions-container">
+          <Search />
+          <Filter />
+        </div>
+        <CreateQuestButton />
       </div>
     </div>
   );
 }
 
-const dummyQuests = {
+function Search() {
+  return (
+    <div className="search">
+      <SearchIcon />
+      <input className="search-input" type="text" placeholder="Search" />
+    </div>
+  );
+}
+function Filter() {
+  const [filter, setFilter] = useState('Near You');
 
+  return (
+    <button className="filter-button">
+      <DropDownIcon />
+      {filter}
+    </button>
+  );
+}
+
+function CreateQuestButton() {
+  return (
+    <button className="create-button">
+      <CreateQuestIcon /> 
+      Create Quest
+    </button>
+  );
 }
 
 export default MainPage;

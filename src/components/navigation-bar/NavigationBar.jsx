5a9042a28560f2b2410465b06face6953c74c20d
sahdeek <sahdeek.thompson@gmail.com>
2024-09-29 18:16:30 -0500

    update navigation bar

--- src/components/navigation-bar/NavigationBar.jsx
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './NavigationBar.css';
 import { useUser } from '../UserProvider'; // Import the useUser hook
 
 import HomeIcon from '../../assets/icons/home_24dp_E8EAED_FILL1_wght400_GRAD0_opsz24.svg?react';
 import CameraIcon from '../../assets/icons/photo_camera_24dp_E8EAED_FILL1_wght400_GRAD0_opsz24.svg?react';
 import SocialIcon from '../../assets/icons/supervised_user_circle_24dp_E8EAED_FILL1_wght400_GRAD0_opsz24.svg?react';
-import QuestsIcon from '../../assets/icons/not_listed_location_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg?react'
+import QuestsIcon from '../../assets/icons/not_listed_location_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg?react';
 
 function NavigationBar() {
   const [currPage, setCurrPage] = useState('/home');
@@ -21,38 +21,42 @@ function NavigationBar() {
         <NavigationButton link={'camera'}>
           <CameraIcon />
         </NavigationButton>
-        <NavigationButton link={'profile'}>
+        <NavigationButton link={'profile'} replace>
           {/* Profile icon with user's image */}
-          <div className='profile-icon'>
+          <div className="profile-icon">
             <img
               src={currentUser?.profilePhoto || '/default-profile-image.jpg'}
               alt={currentUser?.name || 'User Profile'}
-              className='profile-photo'
+              className="profile-photo"
             />
           </div>
         </NavigationButton>
-        <NavigationButton link={'connections'}>
+        <NavigationButton link={'connections'} replace>
           <SocialIcon />
         </NavigationButton>
-        <NavigationButton link={'quests'}>
+        <NavigationButton link={'quests'} replace>
           <QuestsIcon />
         </NavigationButton>
       </div>
     </div>
   );
 
-  function NavigationButton({ link, children }) {
+  function NavigationButton({ link, children, replace = false }) {
     const isSelected = link === currPage;
 
     return (
-      <Link
-        className={`navigation-bar-button ${isSelected ? 'selected' : ''}`}
+      <NavLink
+        end
+        className={({ isActive }) => {
+          return `navigation-bar-button ${isActive ? 'selected' : ''}`;
+        }}
         id={link}
         to={link}
+        replace={replace}
         onClick={() => setCurrPage(link)}
       >
         {children}
-      </Link>
+      </NavLink>
     );
   }
 }

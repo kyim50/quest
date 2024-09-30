216c037bdd6b04e2e256b3967303bfdcb8b9044e
kyim50 <115094752+kyim50@users.noreply.github.com>
2024-09-30 01:14:40 -0500

    update index.html

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  build: {
-    outDir: 'dist', // Default is 'dist', you can change it if necessary
+  plugins: [react()],
+  resolve: {
+    // Remove any aliasing that points to index.js or similar
+    alias: {
+      // Example of an alias that might need removing
+      // 'react-responsive': path.resolve(__dirname, 'node_modules/react-responsive'),
+    },
   },
 });
